be69e71480b9d8c39af5de5d0783558bec2d514e
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-09 12:57:54 +0530

    basic sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,28 @@
-<script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div
+	class="d-flex flex-column align-items-stretch flex-shrink-0 bg-white"
+	style="width: 380px;"
+>
+	<a
+		href="/"
+		class="d-flex align-items-center flex-shrink-0 p-3 link-dark text-decoration-none border-bottom"
+	>
+		<span class="fs-5 fw-semibold">List group</span>
+	</a>
+	<div class="list-group list-group-flush border-bottom scrollarea">
+		<a
+			href="#"
+			class="list-group-item list-group-item-action py-3 lh-tight"
+		>
+			<div
+				class="d-flex w-100 align-items-center justify-content-between"
+			>
+				<strong class="mb-1">List group item heading</strong>
+				<small class="text-muted">Tues</small>
+			</div>
+			<div class="col-10 mb-1 small">
+				Some placeholder content in a paragraph below the heading and
+				date.
+			</div>
+		</a>
+	</div>
+</div>
