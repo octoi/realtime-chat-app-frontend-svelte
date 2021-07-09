3ad47ff02756b34b7ae0a4c80c69abd292ceef6d
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-09 13:04:07 +0530

    looping through messages

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	let username = "username";
+	let messages = [];
 </script>
 
 <div class="container">
@@ -14,18 +15,18 @@
 			/>
 		</div>
 		<div class="list-group list-group-flush border-bottom scrollarea">
-			<div class="list-group-item list-group-item-action py-3 lh-tight">
+			{#each messages as msg}
 				<div
-					class="d-flex w-100 align-items-center justify-content-between"
+					class="list-group-item list-group-item-action py-3 lh-tight"
 				>
-					<strong class="mb-1">List group item heading</strong>
-					<small class="text-muted">Tues</small>
+					<div
+						class="d-flex w-100 align-items-center justify-content-between"
+					>
+						<strong class="mb-1">{msg.username}</strong>
+					</div>
+					<div class="col-10 mb-1 small">{msg.message}</div>
 				</div>
-				<div class="col-10 mb-1 small">
-					Some placeholder content in a paragraph below the heading
-					and date.
-				</div>
-			</div>
+			{/each}
 		</div>
 	</div>
 </div>
