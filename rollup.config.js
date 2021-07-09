aee93e2adff94c333fb43afadb7b69cb77cb3f6b
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-09 13:16:01 +0530

    env

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,10 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import { config } from 'dotenv';
 import css from 'rollup-plugin-css-only';
+import replace from '@rollup/plugin-replace';
+
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -37,6 +40,14 @@ export default {
 		file: 'public/build/bundle.js'
 	},
 	plugins: [
+		replace({
+			__myapp: JSON.stringify({
+				env: {
+					isProd: production,
+					...config().parsed, // attached the .env config
+				}
+			}),
+		}),
 		svelte({
 			compilerOptions: {
 				// enable run-time checks when not in production
